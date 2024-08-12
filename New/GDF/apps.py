5af2b7765832097cc16c65ea99619f6333bc6f16
Gururaj8756 <107394510+Gururaj8756@users.noreply.github.com>
2024-08-12 12:58:59 +0530

    Delete New directory

--- New/GDF/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GdfConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GDF'
--- New/GDF/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class UserName(models.Model):
-    session_id = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
